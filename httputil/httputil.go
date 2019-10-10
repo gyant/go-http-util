7d28bd1dc5165d5ffb23d49b0024f064262d7864
Ryan Thompson <ryan@zengyant.com>
2019-10-09 22:27:26 -0700

    Testing

--- httputil/httputil.go
+++ httputil/httputil.go
@@ -1,58 +1,53 @@
 package httputil
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
 )
 
 type APIClient interface {
-	getBaseURL() *url.URL
-	getHTTPClient() *http.Client
+	// getBaseURL() *url.URL
+	// getHTTPClient() *http.Client
 	getUserAgent() string
 }
 
 type HTTPUtil struct{}
 
-func (h *HTTPUtil) NewRequest(c APIClient, method, path string, body interface{}) (*http.Request, error) {
-	rel := &url.URL{Path: path}
-	u := c.getBaseURL().ResolveReference(rel)
-	var buf io.ReadWriter
-	if body != nil {
-		buf = new(bytes.Buffer)
-		err := json.NewEncoder(buf).Encode(body)
-		if err != nil {
-			return nil, err
-		}
-	}
-	req, err := http.NewRequest(method, u.String(), buf)
-	if err != nil {
-		return nil, err
-	}
-	if body != nil {
-		req.Header.Set("Content-Type", "application/json")
-	}
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("User-Agent", c.getUserAgent())
-	return req, nil
-}
+// func (h *HTTPUtil) NewRequest(c APIClient, method, path string, body interface{}) (*http.Request, error) {
+// 	rel := &url.URL{Path: path}
+// 	u := c.getBaseURL().ResolveReference(rel)
+// 	var buf io.ReadWriter
+// 	if body != nil {
+// 		buf = new(bytes.Buffer)
+// 		err := json.NewEncoder(buf).Encode(body)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 	}
+// 	req, err := http.NewRequest(method, u.String(), buf)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	if body != nil {
+// 		req.Header.Set("Content-Type", "application/json")
+// 	}
+// 	req.Header.Set("Accept", "application/json")
+// 	req.Header.Set("User-Agent", c.getUserAgent())
+// 	return req, nil
+// }
 
-func (h *HTTPUtil) Do(c APIClient, req *http.Request, v interface{}) (*http.Response, error) {
-	resp, err := c.getHTTPClient().Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+// func (h *HTTPUtil) Do(c APIClient, req *http.Request, v interface{}) (*http.Response, error) {
+// 	resp, err := c.getHTTPClient().Do(req)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer resp.Body.Close()
 
-	if v != nil {
-		err = json.NewDecoder(resp.Body).Decode(v)
-	}
+// 	if v != nil {
+// 		err = json.NewDecoder(resp.Body).Decode(v)
+// 	}
 
-	return resp, err
-}
+// 	return resp, err
+// }
 
 func (h *HTTPUtil) Test(c APIClient) {
 	fmt.Println("OH HI FROM HTTPUTIL TEST.")
