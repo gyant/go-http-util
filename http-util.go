04effa7fbf0fdca8a10cf42993008ff15896e928
Ryan Thompson <ryan@zengyant.com>
2019-10-09 20:53:31 -0700

    Initial commit.

---
+++ http-util.go
@@ -0,0 +1,56 @@
+package httputil
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type APIClient interface {
+}
+
+type HTTPUtil struct{}
+
+func (h *HTTPUtil) newRequest(c APIClient, method, path string, body interface{}) (*http.Request, error) {
+	rel := &url.URL{Path: path}
+	u := c.BaseURL.ResolveReference(rel)
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", c.UserAgent)
+	return req, nil
+}
+
+func (h *HTTPUtil) do(c APIClient, req *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if v != nil {
+		err = json.NewDecoder(resp.Body).Decode(v)
+	}
+
+	return resp, err
+}
+
+func (h *HTTPUtil) test() {
+	fmt.Println("OH HI FROM HTTPUTIL TEST.")
+}
